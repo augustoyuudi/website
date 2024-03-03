e6db51f3054f75a0b7b2e10ddb76e7f313e5774c
augustoyuudi <augustoyuudi@gmail.com>
2024-03-03 10:24:47 -0300

    feat: add placeholder portfolio

---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+export default function Home() {
+  return (
+    <main className="px-[10vw] py-[5vh]">
+      <div className='text-balance text-[#313131] max-w-3xl mt-9'>
+        <h1 className='text-6xl text-medium mb-8'>
+          Hello.
+        </h1>
+
+        <h2 className='text-3xl text-medium mb-8'>
+          My name is Augusto Yuudi Shiraishi.
+        </h2>
+
+        <p className='text-xl font-light mb-4'>
+          I am a software developer based in Brazil. My expertise lies in crafting web apps employing cutting-edge technologies.
+        </p>
+
+        <p className='text-xl font-light mb-4'>
+          In my spare time, I'm immersed in the development of <a className="underlined-link" href="https://github.com/augustoyuudi/potaku" target="_blank">Potaku</a>, a vibrant website designed exclusively for anime enthusiasts.
+        </p>
+
+        <p className='text-xl font-light mb-4'>
+          You can find most of my side projects in my <a className="underlined-link" href="https://github.com/augustoyuudi" target="_blank">Github</a> page. Feel free to reach out to me on <a className="underlined-link" href="https://www.linkedin.com/in/augustoyuudi/" target="_blank">Linkedin</a> or drop a message at <a className='underlined-link' href="mailto:augustoyuudi@gmail.com">augustoyuudi@gmail.com</a>
+        </p>
+
+        <p className='text-xl font-light'>
+          Outside of programming I enjoy traveling ✈️, discovering new culinary experiences 🍜, swimming 🏊, and immersing myself in the captivating worlds of animes 👾 and video games 🎮.
+        </p>
+      </div>
+    </main>
+  );
+}
