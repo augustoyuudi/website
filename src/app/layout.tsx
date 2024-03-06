3c855d63369922ce1c461e20bffd2c38fa0768c8
augustoyuudi <augustoyuudi@gmail.com>
2024-03-06 14:16:46 -0300

    feat: add background color to body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={rubik.className}>{children}</body>
+      <body className={`${rubik.className}, bg-[#F9F9F9]`}>{children}</body>
     </html>
   );
 }
